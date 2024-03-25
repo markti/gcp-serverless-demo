58c29eac302adcbace1aa5753b5c1e9a8ac8fca1
markti <markti@outlook.com>
2024-03-24 21:40:09 -0400

    deployment

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -8,10 +8,29 @@ resource "google_storage_bucket" "backend" {
   depends_on = [google_project_iam_member.terraform_user_storage]
 }
 
+locals {
+  timestamp = timestamp()
+}
+
+resource "random_string" "deployment" {
+  keepers = {
+    date = "${timestamp()}"
+  }
+
+  length = 8
+  special = false
+  upper = false
+
+}
+
 resource "google_storage_bucket_object" "deployment" {
   name   = "deployment.zip"
   bucket = google_storage_bucket.backend.name
   source = "deployment.zip"
+
+    lifecycle {
+      replace_triggered_by = [ random_string.deployment ]
+  }
 }
 
 resource "google_cloudfunctions2_function" "backend" {
