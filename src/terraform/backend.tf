8c68869d9946fdc89cadf2d7661aa120a523353b
markti <markti@outlook.com>
2024-03-24 20:53:36 -0400

    gen2 func

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -14,3 +14,25 @@ resource "google_storage_bucket_object" "deployment" {
   source = "deployment.zip"
 }
 
+resource "google_cloudfunctions2_function" "backend" {
+  name = "func-${var.application_name}-${var.environment_name}-backend-${random_string.project_id.result}"
+  location = var.primary_region
+  description = "a new function"
+
+  build_config {
+    runtime = "dotnet6"
+    entry_point = "Function:HandleAsync"  # Set the entry point 
+    source {
+      storage_source {
+        bucket = google_storage_bucket.backend.name
+        object = google_storage_bucket_object.deployment.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count  = 1
+    available_memory    = "256M"
+    timeout_seconds     = 60
+  }
+}
